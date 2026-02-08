17ab11de1796579450e5a7602c41232b0d8d5881
AntonioGolia-98 <antoniog98@icloud.com>
2026-02-08 18:08:09 +0100

    const footer

---
+++ src/components/products/Topfooter.jsx
@@ -0,0 +1,51 @@
+const footerSection = [
+    {
+        id: 22,
+        title: "DC COMICS",
+        link: [
+            { label: "Characters", href: "#" },
+            { label: "Comics", href: "#" },
+            { label: "Movies", href: "#" },
+            { label: "TV", href: "#" },
+            { label: "Games", href: "#" },
+            { label: "Videos", href: "#" },
+            { label: "News", href: "#" }
+        ],
+    },
+    {
+        id: 32,
+        title: "SHOP",
+        link: [
+            { label: "Shop DC", href: "#" },
+            { label: "Shop DC Collectibles", href: "#" },
+        ],
+    },
+    {
+        id: 42,
+        title: "DC",
+        link: [
+            { label: "Term Of Use", href: "#" },
+            { label: "Privacy policy", href: "#" },
+            { label: "Add Choise", href: "#" },
+            { label: "Adversiting", href: "#" },
+            { label: "Jobs", href: "#" },
+            { label: "Subscription", href: "#" },
+            { label: "Talent Workshop", href: "#" },
+            { label: "CPSC Certificates", href: "#" },
+            { label: "Rating", href: "#" },
+            { label: "Shop Help", href: "#" },
+            { label: "Contacts US", href: "#" }
+        ],
+    },
+    {
+        id: 52,
+        title: "SITES",
+        link: [
+            { label: "DC", href: "#" },
+            { label: "MAD Magazine", href: "#" },
+            { label: "DC Kids", href: "#" },
+            { label: "DC Universe", href: "#" },
+            { label: "DC Power Visa", href: "#" },
+        ],
+    },
+]
