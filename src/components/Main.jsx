f767356bfb17c5711bed8bfa911cd3ebba43cd85
AntonioGolia-98 <antoniog98@icloud.com>
2026-02-08 16:35:23 +0100

    main css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import Mainbar from "./products/Mainbar"
 export default function Main() {
     return (
         <main>
-            <p className="text-center "> content goes here</p>
+            <p className="text-center text-white"> content goes here</p>
             <Mainbar />
         </main>
     )
