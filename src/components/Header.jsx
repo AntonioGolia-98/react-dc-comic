a96b19a0bc886d295e658b6dbd83d05333cdc0c2
AntonioGolia-98 <antoniog98@icloud.com>
2026-02-08 10:51:09 +0100

    creazione header/navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+export default function Header() {
+    return (
+        <header>
+            <figure>
+                <img src="/src/img/dc-logo.png" alt="dc logo" />
+            </figure>
+            <Navbar />
+        </header>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+const Navbar = () => {
+    const links = [
+        { id: 1, text: "CHARACHET", url: "#", current: false },
+        { id: 2, text: "COMICS", url: "#", current: true },
+        { id: 3, text: "MOVIS", url: "#", current: false },
+        { id: 4, text: "TV", url: "#", current: false },
+        { id: 5, text: "GAMES", url: "#", current: false },
+        { id: 6, text: "COLLECTIBLES", url: "#", current: false },
+        { id: 7, text: "VIDEOS", url: "#", current: false },
+        { id: 8, text: "FANS", url: "#", current: false },
+        { id: 9, text: "NEWS", url: "#", current: false },
+        { id: 10, text: "SHOP", url: "#", current: false }
+    ]
+
+
+    const linkList = links.map(links => {
+        return (
+            <li key={links.id}>
+                <a href={links.url}>
+                    {links.text}
+                </a>
+            </li>
+        )
+    });
+
+    return (
+        <nav id="top-navbar" >
+            <ul>
+                {linkList}
+            </ul>
+        </nav >
+    )
+}
+
+export default Navbar
